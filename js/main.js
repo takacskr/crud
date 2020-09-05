6e75e8a8a97d615e7542414f02affacb1fdbecf6
takacskr <takacs.kr@gmail.com>
2020-09-05 21:49:30 +0200

    Delete users fuction is ready.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Get data from the server
-function getServerData(url) {
+let getServerData = (url = "http://localhost:3000/users") => {
     let fetchOptions = {
         method: "GET",
         mode: "cors",
@@ -12,14 +12,16 @@ function getServerData(url) {
     );
 }
 
-document.querySelector("#getDataBtn").addEventListener("click", function() {
-    getServerData("http://localhost:3000/users").then(
+let startGetUsers = _ => {
+    getServerData().then(
         data => fillDataTable(data, "userTable")
     );
-});
+}
+
+document.querySelector("#getDataBtn").addEventListener("click", startGetUsers);
 
 // Fill table with server data
-function fillDataTable(data, tableID) {
+let fillDataTable = (data, tableID) => {
     let table = document.querySelector(`#${tableID}`);
     if (!table) {
         console.error(`Table "${tableID}" is not found.`);
@@ -34,14 +36,52 @@ function fillDataTable(data, tableID) {
             td.innerHTML = row[k];
             tr.appendChild(td);
         }
+        let btnGroup = createBtnGroup();
+        tr.appendChild(btnGroup);
         tBody.appendChild(tr)
     }
 }
 
-function createAnyElement(name, attributes) {
+let createAnyElement = (name, attributes) => {
     let element = document.createElement(name);
     for (let k in attributes) {
         element.setAttribute(k, attributes[k]);
     }
     return element;
+}
+
+let createBtnGroup = _ => {
+    let group = createAnyElement("div", {class: "btn btn-group"});
+    let infoBtn = createAnyElement("button", {class: "btn btn-info", onclick: "getInfo()"});
+    infoBtn.innerHTML = '<i class="fa fa-refresh" aria-hidden="true"></i>';
+    
+    let delBtn = createAnyElement("button", {class: "btn btn-danger", onclick: "delRow(this)"});
+    delBtn.innerHTML = '<i class="fa fa-trash" aria-hidden="true"></i>';
+
+    group.appendChild(infoBtn);
+    group.appendChild(delBtn);
+
+    let td = createAnyElement("td");
+    td.appendChild(group);
+
+    return td;
+}
+
+let delRow = (btn) => {
+    let tr = btn.parentElement.parentElement.parentElement;
+    let id = tr.querySelector("td:first-child").innerHTML;
+    let fetchOptions = {
+        method: "DELETE",
+        mode: "cors",
+        cache: "no-cache"
+    };
+
+    fetch(`http://localhost:3000/users/${id}`, fetchOptions).then(
+        resp => resp.json(),
+        err => console.error(err)
+    ).then(
+        data => {
+            startGetUsers();
+        }
+    );
 }
